47edbdfdb071d3f676af6ef1f1c5c3c4de09349c
BilalRehman08 <bilalrehman080808@gmail.com>
2021-08-03 14:42:21 -0700

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const { request, response } = require("express")
 const express = require("express")
+const mongoose = require("mongoose")
+
+const DB_URI = "mongodb+srv://admin:admin@cluster0.wpjyu.mongodb.net/dev"
+
+mongoose.connect(DB_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+
+// mongoose.connection.on("connected", console.log("Connected"))
+// mongoose.connection.on("error", (error) => console.log(`Error: ${error.message}`))
+
+
+
 const app = express()
 const port = 9000
 
@@ -22,6 +37,7 @@ app.get("/user", (request, response) => {
 app.get("/contact", (request, response) => {
     response.send("Contact Page")
 })
+
 
 
 app.listen(port, console.log(`My server is running on port ${port}`))
